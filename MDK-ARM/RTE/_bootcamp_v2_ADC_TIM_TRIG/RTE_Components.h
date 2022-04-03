32aca21e60c226896ce688f62a2cd0939a8fdfc0
nctiA <ncti.akyl00@gmail.com>
2022-04-04 01:18:06 +0300

    STM32
    STM32

---
+++ MDK-ARM/RTE/_bootcamp_v2_ADC_TIM_TRIG/RTE_Components.h
@@ -0,0 +1,21 @@
+
+/*
+ * Auto generated Run-Time-Environment Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'bootcamp_v2_ADC_TIM_TRIG' 
+ * Target:  'bootcamp_v2_ADC_TIM_TRIG' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+/*
+ * Define the Device Header File: 
+ */
+#define CMSIS_device_header "stm32f4xx.h"
+
+
+
+#endif /* RTE_COMPONENTS_H */
